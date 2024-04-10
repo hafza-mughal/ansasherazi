e47ca0966f33c02d919829abda7c36748a3fdcfd
hafza-mughal <156007447+hafza-mughal@users.noreply.github.com>
2024-04-10 20:50:21 +0500

    file

---
+++ elseif.js
@@ -0,0 +1,29 @@
+let a=prompt("Enter Your percentage")
+if(a>=90 && a<=100){
+    console.log("zabardast")
+}
+else if(a>=80 && a<90){
+    console.log("Excellent")
+}
+else if(a>=70 && a<80){
+    console.log("very good")
+}
+else if(a>=60 && a<70){
+    console.log(" vv good")
+}
+else if(a>=50 && a<60){
+    console.log("star good")
+}
+else if(a>=40 && a<50){
+    console.log("star")
+}
+else if(a>=33 && a<40){
+    console.log("pass")
+}
+else if(a>=1 && a<33){
+    console.log("Try again")
+}
+
+else{
+    console.log("Alfazz Durust karain")
+}
